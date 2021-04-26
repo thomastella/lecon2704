f0c059cd89f1a82df212f36692101b87c37a044e
thomastella <44562314+thomastella@users.noreply.github.com>
2021-04-26 08:12:09 +0200

    Update models.py

--- groupD/models.py
+++ groupD/models.py
@@ -34,11 +34,11 @@ class Group(BaseGroup):
     pass
 class Player(BasePlayer):
     group_no = models.IntegerField()
-    E1 = models.StringField(choices=[['A', 'NO CHANGE '], ['B', 'For example, '], ['C', 'In contrast,'], ['D', 'Nevertheless']], label='1. Choose the best answer:')
-    E2 = models.StringField(choices=[['A', 'NO CHANGE'], ['B', 'speaking in a more pragmatic way, '], ['C', 'speaking in a way more pragmatically, '], ['D', 'in a more pragmatic-speaking way,']], label='2. Choose the best answer:')
-    E3 = models.StringField(choices=[['A', 'NO CHANGE '], ['B', 'teaches '], ['C', 'to teach '], ['D', 'and teaching']], label='3. Choose the best answer:')
+    E1 = models.StringField(choices=[['A', 'In broad terms'], ['B', 'For example, '], ['C', 'In contrast,'], ['D', 'Nevertheless']], label='1. Choose the best answer:')
+    E2 = models.StringField(choices=[['A', 'more pragmatically'], ['B', 'speaking in a more pragmatic way, '], ['C', 'speaking in a way more pragmatically, '], ['D', 'in a more pragmatic-speaking way,']], label='2. Choose the best answer:')
+    E3 = models.StringField(choices=[['A', 'teaching'], ['B', 'teaches '], ['C', 'to teach '], ['D', 'and teaching']], label='3. Choose the best answer:')
     E4 = models.StringField(choices=[['A', 'Consequently, philosophy students have been receiving an increasing number of job offers. '], ['B', 'Therefore, because of the evidence, colleges increased their offerings in philosophy. '], ['C', 'Notwithstanding the attractiveness of this course of study, students have resisted majoring in philosophy. '], ['D', 'However, despite its many utilitarian benefits, colleges have not always supported the study of philosophy.']], label='4. Which choice most effectively sets up the information that follows?')
-    E5 = models.StringField(choices=[['A', 'NO CHANGE '], ['B', 'Thus, '], ['C', 'Moreover, '], ['D', 'However, ']], label='5. Choose the best answer:')
+    E5 = models.StringField(choices=[['A', 'Therefore'], ['B', 'Thus, '], ['C', 'Moreover, '], ['D', 'However, ']], label='5. Choose the best answer:')
     Q1 = models.StringField(choices=[['A', '3'], ['B', '5.7'], ['C', '9.5'], ['D', '14.3']], label='6. A pediatrician uses the model h = 3a + 28.6 to estimate the height h of a boy, in inches, in terms of the boy’s age a, in years, between the ages of 2 and 5. Based on the model, what is the estimated increase, in inches, of a boy’s height each year? ')
     Q2 = models.StringField(choices=[['A', '(0, 7) '], ['B', '(1, 7) '], ['C', '(7, 7)'], ['D', '(14, 2)']], label='7. A line in the xy-plane passes through the origin and has a slope of 1/7 . Which of the following points lies on the line? ')
     Q3 = models.StringField(choices=[['A', '115'], ['B', '120'], ['C', '124'], ['D', '173']], label='8. Katarina is a botanist studying the production of pears by two types of pear trees. She noticed that Type A trees produced 20 percent more pears than Type B trees did. Based on Katarina’s observation, if the Type A trees produced 144 pears, how many pears did the Type B trees produce?')
